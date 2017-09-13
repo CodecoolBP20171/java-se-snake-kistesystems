79154e2b42a78cbf13bd6c70e588867824f86de1
Viktor Matrai <viktor0matrai@gmail.com>
2017-09-13 18:23:05 +0200

    Game is pausing on P key pressed

--- src/com/codecool/snake/GameLoop.java
+++ src/com/codecool/snake/GameLoop.java
@@ -2,9 +2,12 @@ package com.codecool.snake;
 
 import com.codecool.snake.entities.GameEntity;
 import com.codecool.snake.entities.Animatable;
-import com.codecool.snake.entities.enemies.SimpleEnemy;
-import com.codecool.snake.entities.enemies.SkullEnemy;
 import javafx.animation.AnimationTimer;
+import javafx.scene.control.Dialog;
+import javafx.stage.PopupWindow;
+import javafx.stage.Window;
+
+import java.awt.*;
 
 public class GameLoop extends AnimationTimer {
 
@@ -13,15 +16,23 @@ public class GameLoop extends AnimationTimer {
     public void handle(long now) {
         for (GameEntity gameObject : Globals.gameObjects) {
             if (gameObject instanceof Animatable) {
-                Animatable animObject = (Animatable)gameObject;
+                Animatable animObject = (Animatable) gameObject;
                 animObject.step();
             }
-
         }
         Globals.gameObjects.addAll(Globals.newGameObjects);
         Globals.newGameObjects.clear();
 
         Globals.gameObjects.removeAll(Globals.oldGameObjects);
         Globals.oldGameObjects.clear();
+
+        if (Globals.pKeyPressed) {
+            Globals.gameLoop.stop();
+            Dialog popUp = new Dialog();
+            popUp.initOwner(Snake.getPrimaryStageRef());
+            popUp.showAndWait();
+            Globals.pKeyPressed = false;
+        }
     }
 }
+
--- src/com/codecool/snake/Snake.java
+++ src/com/codecool/snake/Snake.java
@@ -1,44 +1,27 @@
 package com.codecool.snake;
 
-import com.sun.prism.paint.ImagePattern;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.layout.*;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.scene.paint.Color;
+import javafx.stage.Popup;
 import javafx.stage.Stage;
 
-import java.io.File;
-
 public class Snake extends Application {
-    MediaPlayer mediaPlayer;
-    public void backgroundMusic(String musicFile){
-        Media sound = new Media(new File(musicFile).toURI().toString());
-        mediaPlayer = new MediaPlayer(sound);
-        mediaPlayer.setVolume(0.2f);
-        mediaPlayer.play();
-    }
+
+    private static Stage primaryStageRef;
+
     @Override
     public void start(Stage primaryStage) {
         Game game = new Game();
-
-        backgroundMusic("BackGround.mp3");
-
-        Scene scene = new Scene(game, Globals.WINDOW_WIDTH, Globals.WINDOW_HEIGHT);
-        scene.setFill(Color.GRAY);
-
-        BackgroundImage myBI= new BackgroundImage(new Image("Space.jpg",Globals.WINDOW_WIDTH,Globals.WINDOW_HEIGHT,false,true),
-                BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
-                BackgroundSize.DEFAULT);
-//then you set to your node
-        game.setBackground(new Background(myBI));
+        primaryStageRef = primaryStage;
 
         primaryStage.setTitle("Snake Game");
-        primaryStage.setScene(scene);
+        primaryStage.setScene(new Scene(game, Globals.WINDOW_WIDTH, Globals.WINDOW_HEIGHT));
         primaryStage.show();
         game.start();
+    }
+
+    public static Stage getPrimaryStageRef() {
+        return primaryStageRef;
     }
 
 }
